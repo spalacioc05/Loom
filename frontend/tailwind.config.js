4b6f304c24c396081bb366057729407150379b64
Santiago Palacio <spalacioc05@gmail.com>
2025-10-16 22:28:19 -0500

    nuevo proyecto

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/pages/**/*.{js,jsx}",
+    "./src/components/**/*.{js,jsx}",
+    "./src/app/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

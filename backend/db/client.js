434c19ec00eced4214c3f9e830f523d1c424fa12
Santiago Palacio <spalacioc05@gmail.com>
2025-10-16 21:13:33 -0500

    actualizacion

--- backend/db/client.js
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config'
-import postgres from 'postgres'
-
-const connectionString = process.env.DATABASE_URL
-const sql = postgres(connectionString, { ssl: 'require' })
-
-export default sql
--- backend/routes/routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getAllBooks } from '../controllers/books_controllers.js';
-
-const router = Router();
-
-router.get('/disponibles', getAllBooks);
-
-export default router;

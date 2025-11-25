0419cd8800f2e7757873d94b2862a3d33fbdd251
saraires <sarairestrepo22@gmail.com>
2025-11-25 03:37:33 -0500

    chore: Clean repository - Remove test files, build artifacts, and node_modules for deployment

--- backend/test_pdf_parse.js
+++
@@ -1,8 +0,0 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-
-const pdfParse = require('pdf-parse');
-
-console.log('Type of pdfParse:', typeof pdfParse);
-console.log('pdfParse:', pdfParse);
-console.log('pdfParse.default:', pdfParse.default);
